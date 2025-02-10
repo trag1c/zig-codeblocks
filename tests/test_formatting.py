0636c801449c63e3da6012b005d9c66311ba45d6
trag1c <trag1cdev@yahoo.com>
2025-02-10 20:52:10 +0100

    test: add syntax highlighting tests

---
+++ tests/test_formatting.py
@@ -0,0 +1,40 @@
+import json
+from pathlib import Path
+
+import pytest
+
+from zig_codeblocks.formatting import process_markdown
+
+SOURCE_DIR = Path(__file__).parent / "sources"
+
+
+def read_expected_styling(test_name: str) -> str:
+    return "".join(
+        json.loads(
+            (SOURCE_DIR / "formatting_results" / f"{test_name}.json").read_bytes()
+        )
+    )
+
+
+@pytest.mark.parametrize(
+    "test_name",
+    [
+        "assign_undefined",
+        "comments",
+        "emoji",
+        "global_assembly",
+        "hello_again",
+        "identifiers",
+    ],
+)
+def test_zig_highlighting(test_name: str) -> None:
+    source = (
+        (SOURCE_DIR / "zig_inputs" / f"{test_name}.zig")
+        .read_bytes()
+        .replace(b"\r\n", b"\n")
+        .decode()
+    )
+    expected_styling = f"```ansi\n{read_expected_styling(test_name)}\n```"
+    # print(">", process_markdown(f"```zig\n{source}```"))
+    # print("<", expected_styling)
+    assert process_markdown(f"```zig\n{source}```", only_code=True) == expected_styling
